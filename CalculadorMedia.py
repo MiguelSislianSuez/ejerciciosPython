e2ebcd41e988d5f8039c5d8e7b765560fd54b9a1
Miguel Alejandro Sislian Suez <52850875+MiguelSislianSuez@users.noreply.github.com>
2021-01-16 22:48:28 +0100

    Add files via upload

---
+++ CalculadorMedia.py
@@ -0,0 +1,10 @@
+'''Calcular la media de tres valores introducidos por teclado'''
+
+val1 = int(input('Introduzca primera nota: '))
+val2 = int(input('Introduzca segunda nota: '))
+val3 = int(input('Introduzca tercera nota: '))
+
+resultado = val1+val2+val3
+media = resultado / 3
+
+print('La media de las tres notas es: ', media)
