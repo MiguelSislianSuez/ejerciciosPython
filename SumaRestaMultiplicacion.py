fe2a5bc077b44b4687a85a88aa00caee6c97f617
Miguel Alejandro Sislian Suez <52850875+MiguelSislianSuez@users.noreply.github.com>
2021-01-16 22:23:54 +0100

    Add files via upload

---
+++ SumaRestaMultiplicacion.py
@@ -0,0 +1,10 @@
+import math
+'''Dados dos números, mostrar la suma, resta, división y multiplicación de ambos.'''
+
+num1 = int(input('Introduzca primer número: '))
+num2 = int(input('Introduzca segundo número: '))
+print("La suma es = ",num1 + num2,
+      "\nLa resta es = ",num1-num2,
+      "\nLa multiplicación es = ",num1*num2,
+      "\nLa división es = ", num1/num2)
+
