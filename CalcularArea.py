bfc73483adb482306bf55f2291bfe380a45fe36e
Miguel Alejandro Sislian Suez <52850875+MiguelSislianSuez@users.noreply.github.com>
2021-01-16 22:26:41 +0100

    Add files via upload

---
+++ CalcularArea.py
@@ -0,0 +1,10 @@
+'''Ejercicio 2
+Calcular el perímetro y área de un rectángulo dada su base y su altura. Base 3 altura 2.
+Formula P=2*(h+b)
+'''
+
+base=int(input('Introduzca base: '))
+altura=int(input('Introduzca altura: '))
+
+perimetro=2*(base+altura)
+print('El area del rectángulo es: ', perimetro)
